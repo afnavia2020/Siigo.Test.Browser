fd8e516dec7a435b2517f80a4ace2272561f9c6b
afnavia2020 <75805494+afnavia2020@users.noreply.github.com>
2021-02-24 12:13:36 -0500

    protocol

--- msBrowser/src/test/scala/GetConfiguration.scala
+++ msBrowser/src/test/scala/GetConfiguration.scala
@@ -12,14 +12,7 @@ class GetConfiguration extends Simulation{
   )
   val httpProtocol = http
 		.baseUrl("https://servicesqa.siigo.com/catalog/api/v1/Autocomplete")
-		.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
-		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-		.acceptEncodingHeader("gzip, deflate")
-		.acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
-		.upgradeInsecureRequestsHeader("1")
-		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
-
-
+		
   val tokensFeeder = csv("tokens.csv").circular
 
   val rnd = new Random()
