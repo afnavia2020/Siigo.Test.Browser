519288c6b8d97cdd39bd3ede2945cddf1cd691d3
afnavia2020 <75805494+afnavia2020@users.noreply.github.com>
2021-02-24 12:37:48 -0500

    subiendo protocolo

--- msBrowser/src/test/scala/GetConfiguration.scala
+++ msBrowser/src/test/scala/GetConfiguration.scala
@@ -13,7 +13,13 @@ class GetConfiguration extends Simulation{
   val httpProtocol = http
 		.baseUrl("https://servicesqa.siigo.com/catalog/api/v1/Autocomplete")
     .baseUrl("https://servicesqa.siigo.com/auth/connect/token?")
-		
+    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
+		.upgradeInsecureRequestsHeader("1")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
   val tokensFeeder = csv("tokens.csv").circular
 
   val rnd = new Random()
@@ -48,7 +54,7 @@ class GetConfiguration extends Simulation{
   setUp(
     getConfigurationScenario.inject(
       atOnceUsers(5),
-      rampUsersPerSec(5) to (100) during(15 minutes)
+      rampUsersPerSec(5) to (10) during(3 minutes)
     )
   )
 
