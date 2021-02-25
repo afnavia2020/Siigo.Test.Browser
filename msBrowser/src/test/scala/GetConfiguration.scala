2276044b4e4fc35b87035845c5d6f0f8b379adb6
afnavia2020 <75805494+afnavia2020@users.noreply.github.com>
2021-02-24 19:07:53 -0500

    change 1

--- msBrowser/src/test/scala/GetConfiguration.scala
+++ msBrowser/src/test/scala/GetConfiguration.scala
@@ -19,7 +19,7 @@ class GetConfiguration extends Simulation{
 		.upgradeInsecureRequestsHeader("1")
 		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
 
-  val tokensFeeder = csv("tokens.csv").circular
+  val tokensFeeder = csv("data/tokens.csv").circular
                   
 
   val rnd = new Random()
